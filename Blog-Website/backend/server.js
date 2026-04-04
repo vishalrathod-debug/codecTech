a978f5d9bd847fc1317376c5f517f0eaedaea989
vishalrathod-debug <vishalrathod960405@gmail.com>
2026-04-04 21:39:03 +0530

    Set up Express server with CORS and routes

--- Blog-Website/backend/server.js
+++ Blog-Website/backend/server.js
@@ -1,1 +1,18 @@
-console.log('Blog backend running');
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const connectDB = require('./config/db');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+connectDB();
+
+app.use('/api/auth', require('./routes/authRoutes'));
+app.use('/api/posts', require('./routes/postRoutes'));
+
+app.listen(process.env.PORT, () =>
+  console.log(`Server running on ${process.env.PORT}`)
+);
