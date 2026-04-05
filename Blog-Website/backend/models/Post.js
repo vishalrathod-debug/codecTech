e5eebf5cdf03921ace91101da9c2f083caf6d912
vishalrathod-debug <vishalrathod960405@gmail.com>
2026-04-05 22:31:05 +0530

    Add validation to Post model fields

--- Blog-Website/backend/models/Post.js
+++ Blog-Website/backend/models/Post.js
@@ -2,14 +2,17 @@ const mongoose = require('mongoose');
 const slugify = require('slugify');
 
 const PostSchema = new mongoose.Schema({
-  title: String,
+  title: { type: String, required: [true, 'Title is required'] },
   slug: { type: String, unique: true },
-  content: String,
-  author: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }
+  content: { type: String, required: [true, 'Content is required'] },
+  author: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }
 }, { timestamps: true });
 
+// Auto-create slug from title
 PostSchema.pre('save', function(next) {
-  this.slug = slugify(this.title, { lower: true });
+  if (this.isModified('title')) {
+    this.slug = slugify(this.title, { lower: true, strict: true });
+  }
   next();
 });
 
